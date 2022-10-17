ff95fb79ad6100a88bb91dad7f7192b0ebbf49be
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-10-17 12:01:01 +0300

    feat: added some lines to tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,56 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.data) return null;
+    return this.data[0];
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let newNode = new Node(data);
+    if (!this.data) {
+        this.data = newNode;
+    } else {
+        if (newNode < this.data) {
+          if (!this.left) {
+            this.left = newNode;
+          } else {
+            this.left.add(newNode);
+          }
+        } else {
+          if (this.right === null) {
+            this.right = newNode;
+          } else {
+            this.right.add(newNode);
+          }
+        }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 }
 
